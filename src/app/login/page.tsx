7743bb68ded93c11ddd2bd98b51b761b737b18bb
Namoral Thy <thynamoral18@gmail.com>
2025-03-20 02:24:36 +0700

    [add] login page metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
 import LoginPage from "@/components/pages/login/LoginPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Log in | Momo E-Commerce",
+  description: "Momo E-Commerce login page.",
+};
 
 export default function Login() {
   return <LoginPage />;
