75ed30a69ab2eda271d6b66b8a8754d5a8c3eb9d
Namoral Thy <thynamoral18@gmail.com>
2025-03-24 00:49:27 +0700

    [add] env config to access backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
+import dotenv from "dotenv";
+import process from "process";
+
+dotenv.config({ path: ".env.local" });
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    if (!process.env.BACKEND_URL) {
+      throw new Error(
+        "BACKEND_URL is not defined in the environment variables."
+      );
+    }
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL, // Expose BACKEND_URL to Next.js
+  },
+};
 
 export default nextConfig;
