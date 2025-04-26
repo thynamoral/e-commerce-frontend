b9caa1129bd477474544ddadd18241a41463d4ea
Namoral Thy <thynamoral18@gmail.com>
2025-04-26 13:26:46 +0700

    [add] mock product card

--- src/components/app/home/home-products.tsx
+++ src/components/app/home/home-products.tsx
@@ -1,11 +1,19 @@
+import ProductCard from "@/components/ui/product-card";
+
 export default function HomeProducts() {
   return (
     <div className="flex-1 p-4 space-y-3 bg-white rounded-md">
       <h2 className="font-bold">All Products</h2>
-      <div className="grid grid-cols-3 space-x-3">
-        <div>test</div>
-        <div>test</div>
-        <div>test</div>
+      <div className="grid grid-cols-3 gap-x-3 gap-y-6">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </div>
   );
