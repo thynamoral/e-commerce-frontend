42f4c51fa9fb93fbb0f9e8677c8b7c63fa5dc3f4
Namoral Thy <thynamoral18@gmail.com>
2025-04-26 13:02:51 +0700

    [init] home products component

---
+++ src/components/app/home/home-products.tsx
@@ -0,0 +1,12 @@
+export default function HomeProducts() {
+  return (
+    <div className="flex-1 p-4 space-y-3 bg-white rounded-md">
+      <h2 className="font-bold">All Products</h2>
+      <div className="grid grid-cols-3 space-x-3">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </div>
+  );
+}
