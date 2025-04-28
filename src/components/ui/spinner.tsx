278e1e0c79ede1f76b7002191cc1c54d110ea691
Namoral Thy <thynamoral18@gmail.com>
2025-04-28 17:10:16 +0700

    [add] spinner component

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,9 @@
+import { LoaderCircle } from "lucide-react";
+
+const Spinner = () => (
+  <>
+    <LoaderCircle className="animate-spin h-12 w-12 text-black-2" />
+  </>
+);
+
+export default Spinner;
