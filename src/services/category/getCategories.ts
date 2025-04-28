f920f0415091fa80c115e1db7b14fc1b0459e5bd
Namoral Thy <thynamoral18@gmail.com>
2025-04-28 14:40:22 +0700

    [add] getCategories service

---
+++ src/services/category/getCategories.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchApi } from "../api";
+
+type CategoryResponse = {
+  category_id: string;
+  category_name: string;
+  slug: string;
+  createdat: Date;
+  updatedat: Date;
+};
+
+const getCategories = async () => {
+  try {
+    const response = await fetchApi<CategoryResponse[]>("/categories");
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const useGetCategories = () => {
+  return useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+};
