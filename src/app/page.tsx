de5726bd37074f4a4f20df93c8fbf1673674b4f4
Namoral Thy <thynamoral18@gmail.com>
2025-05-03 18:39:32 +0700

    [fix] build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import HomeProducts from "@/components/app/home/home-products";
 import ContainerWrapper from "@/components/ui/container-wrapper";
 import SearchAndFilter from "@/components/app/home/search-and-filter";
 import SearchAndFilterFormProvider from "@/providers/search-and-filter-provider";
+import { Suspense } from "react";
+import Spinner from "@/components/ui/spinner";
 
 export default function Home() {
   return (
-    <SearchAndFilterFormProvider>
-      <ContainerWrapper className="lg:min-h-full flex gap-4">
-        <SearchAndFilter />
-        <HomeProducts />
-      </ContainerWrapper>
-    </SearchAndFilterFormProvider>
+    <Suspense fallback={<Spinner />}>
+      <SearchAndFilterFormProvider>
+        <ContainerWrapper className="lg:min-h-full flex gap-4">
+          <SearchAndFilter />
+          <HomeProducts />
+        </ContainerWrapper>
+      </SearchAndFilterFormProvider>
+    </Suspense>
   );
 }
