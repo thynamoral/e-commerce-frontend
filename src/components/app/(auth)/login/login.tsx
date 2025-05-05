0f9c829e95c223897cac0b17de0b449a1e803835
Namoral Thy <thynamoral18@gmail.com>
2025-05-05 16:37:38 +0700

    [update] login page: redirect if user is authenticated

--- src/components/app/(auth)/login/login.tsx
+++ src/components/app/(auth)/login/login.tsx
@@ -1,6 +1,20 @@
+"use client";
+import * as React from "react";
+import { useAuth } from "@/hooks/useAuth";
 import LoginForm from "./login-form";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  // useEffect
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      router.replace("/");
+    }
+  }, [isAuthenticated]);
+
   return (
     <div className="max-w-[400px] w-full bg-background shadow-md rounded-[16px] px-10 py-6 space-y-8">
       <header className="text-center space-y-4">
